a6eab5f951aded48468796f35ba4d464c64a88c5
Rupali Sharma <rupalisharma1009@gmail.com>
2026-05-09 19:29:44 +0530

    added frontend deployment changes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -28,7 +28,7 @@ const PORT = process.env.PORT || 5000
 
 connectDB()
 
-app.use(cors({ origin: process.env.CLIENT_URL }))
+app.use(cors({ origin: process.env.FRONTEND_URL}))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
